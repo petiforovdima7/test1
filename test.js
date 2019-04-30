221c31110c90a1501b920f81a7cb021861d26e8f
petiforovdima7 <petiforovdima7@gmail.com>
2019-04-30 13:30:51 +0300

    Добавил домашку

--- test.js
+++ test.js
@@ -1,22 +1,49 @@
 
-var money = prompt('Ваш бюджет в месяц?');
-var time = prompt('Введите дату в формате YYYY-MM-DD');
+let money = +prompt('Ваш бюджет в месяц?', '');
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-var appData =  new Object();
-appData.money = money;
-appData.timeData = time;
+let appData = {
+    budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    timeData: time,
+    savings: false
+};
 
-var expensesSpendThisMons = prompt('Введите обязательную статью расходов в этом месяце');
-var expensesHowMatch = prompt('Во сколько обойдется?');
+// 1
+// for (let i = 0; i < 2; i++) {
+//     let  a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//          b = prompt("Во сколько обойдеться", '');
+    
+//     if ( (typeof(a))=== 'string' && (typeof(a)) != null  && (typeof(a)) != null 
+//     && a != '' && b != '' && a.length < 50) {
+//         console.log("done");
+//         appData.expenses[a] = b;     
+//     }     
+// };
+// 2
+// let i = 0;
+// while (i < 2) {
+//     let  a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//          b = prompt("Во сколько обойдеться", '');
+//          appData.expenses[a] = b;
+//          i++;     
+// };
 
-var expenses = {expensesSpendThisMons, expensesHowMatch};
-var optionalExpenses = {};
-var income = {};
-savings: false;
 
-var moneyForMonth = money/30;
+appData.moneyPerDay = appData.budget / 30;
 
-alert(moneyForMonth);
+alert( "Ежедневный бюджет " + appData.moneyPerDay);
 
+if(appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Ошибка");
+};
 
 
